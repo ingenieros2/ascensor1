0160f4982fb45fdb5ecad7d2973108a351694467
Matias97m <matiasandresmolina97@gmail.com>
2021-03-25 20:45:32 -0300

    ascensor a duras penas

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var ascensor = new Ascensor(6);
+var cantidad = ascensor.filaLlamadas.length
+/*for (var i = 0; i < cantidad; i++) {
+    setTimeout(function() {
+        ascensor.moverAscensor()
+    }, 4000 * i)
+    
+}*/
